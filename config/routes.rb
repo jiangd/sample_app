4289512e15f7d0ea2dfdfaa22dc81f1ee7af689e
Dong <dj.dong.jiang@gmail.com>
2014-03-18 23:29:32 +0800

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -54,11 +54,12 @@ SampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  get "users/new"
 
+  resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
 end
