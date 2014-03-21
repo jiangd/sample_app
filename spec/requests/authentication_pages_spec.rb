0c5a0f1a3fa646088447d3890e6e27470b7b1a62
Dong <dj.dong.jiang@gmail.com>
2014-03-21 22:33:08 +0800

    add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,21 +19,22 @@ describe "Authentication" do
       end
     end
 
-  describe "with valid information" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { sign_in user }
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
 
-    it { should have_title(user.name) }
+      it { should have_title(user.name) }
     it { should have_link('Users',       href: users_path) }
     it { should have_link('Profile',     href: user_path(user)) }
     it { should have_link('Settings',    href: edit_user_path(user)) }
     it { should have_link('Sign out',    href: signout_path) }
     it { should_not have_link('Sign in', href: signin_path) }
     
-    describe "followed by signout" do
+      describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
-    end      
+      end      
+    end
   end
 
   describe "authorization" do
@@ -62,9 +63,19 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
-        	
       end
+      describe "in the Microposts controller" do
 
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
